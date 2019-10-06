650f5842db65a795413668e46ef78cea09d65614
snsinfu <snsinfu@gmail.com>
2019-10-06 23:05:50 +0900

    domain: Add bastion domain
    rio.* is for accessing apps via Cloudflare. rio-bastion.* is for
    directly accessing the server (mainly for wireguard).

--- domain.tf
+++ domain.tf
@@ -1,7 +1,15 @@
-resource "cloudflare_record" "master" {
+resource "cloudflare_record" "rio" {
   domain  = var.domain_zone
   name    = "rio"
   type    = "A"
   value   = "${hcloud_server.master.ipv4_address}"
   proxied = true
+}
+
+resource "cloudflare_record" "rio-bastion" {
+  domain  = var.domain_zone
+  name    = "rio-bastion"
+  type    = "A"
+  value   = "${hcloud_server.master.ipv4_address}"
+  proxied = false
 }
