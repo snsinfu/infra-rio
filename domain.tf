ec6bf87dfd9b01070cfeb5fe0daa397201af757e
snsinfu <snsinfu@gmail.com>
2019-10-20 21:41:26 +0900

    domain: Remove bastion domain
    Just use A record for the `rio` subdomain without proxying. Apply proxy
    on CNAME entries (pointing to the rio subdomain) for individual services.

--- domain.tf
+++ domain.tf
@@ -3,13 +3,5 @@ resource "cloudflare_record" "rio" {
   name    = "rio"
   type    = "A"
   value   = "${hcloud_server.master.ipv4_address}"
-  proxied = true
-}
-
-resource "cloudflare_record" "rio-bastion" {
-  domain  = var.domain_zone
-  name    = "rio-bastion"
-  type    = "A"
-  value   = "${hcloud_server.master.ipv4_address}"
   proxied = false
 }
